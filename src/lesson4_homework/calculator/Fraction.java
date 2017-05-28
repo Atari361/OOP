a78e2aef0750f12fa320a705a0f7ede0c2daa80b
Atari361 <letsky1994@gmail.com>
2017-05-28 21:23:31 +0300

    lesson4_hw_part3

---
+++ src/lesson4_homework/calculator/Fraction.java
@@ -0,0 +1,42 @@
+package lesson4_homework.calculator;
+
+/**
+ * Created by letsk on 05/28/2017.
+ */
+public class Fraction {
+
+    double number;
+
+    public Fraction(double number) {
+        this.number = number;
+    }
+
+    public Fraction add(Fraction frac){
+        return new Fraction(number + frac.get());
+    }
+
+    public Fraction sub(Fraction frac){
+        return new Fraction(number - frac.get());
+    }
+
+    public Fraction mul(Fraction frac){
+        return new Fraction(number / frac.get());
+    }
+
+    public Fraction div(Fraction frac){
+        return new Fraction(number * frac.get());
+    }
+
+    public String toString() {
+        return "" + number;
+    }
+
+    public  void print() {
+        System.out.println(number);
+    }
+
+    public double get() {
+        return number;
+    }
+
+}
--- src/lesson4_homework/calculator/Main.java
+++ src/lesson4_homework/calculator/Main.java
@@ -9,12 +9,28 @@ public class Main {
 
         Number number4 = new Number(4);
         Number number2 = new Number(2);
-        System.out.println("4 plus 2 is: " + number4.add(number2).get());
-        System.out.println("4 minus 2 is: " + number4.sub(number2).get());
-        System.out.println("4 multiply 2 is: " + number4.mul(number2).get());
-        System.out.println("4 divide 2 is: " + number4.div(number2).get());
-        System.out.println("2 power of 4 is: " + number2.pow(number4).get());
-        System.out.println("2 mod of 4 is: " + number2.mod(number4).get());
-        System.out.println("Factorial of 4 is : " + number4.fact().get());
+
+        System.out.println(number4.get() + " plus " + number2.get() + " is: " + number4.add(number2).get());
+        System.out.println(number4.get() + " minus " + number2.get() + " is: " + number4.sub(number2).get());
+        System.out.println(number4.get() + " multiply to  " + number2.get() + " is: " + number4.mul(number2).get());
+        System.out.println(number4.get() + " divide of " + number2.get() + " is: " + number4.div(number2).get());
+        System.out.println(number2.get() + " power of " + number4.get() + " is: " + number2.pow(number4).get());
+        System.out.println(number2.get() + " mod of " + number4.get() + " is: " + number2.mod(number4).get());
+        System.out.println("Factorial of " + number4.get() + " is : " + number4.fact().get());
+
+        System.out.println("\n===========================\n");
+
+        Fraction fractionNumber4and123 = new Fraction(4.123);
+        Fraction fractionNumber2and246 = new Fraction(2.246);
+
+        System.out.println(fractionNumber4and123.get() + " plus " + fractionNumber2and246.get() + " is: " +
+                fractionNumber4and123.add(fractionNumber2and246).get());
+        System.out.println(fractionNumber4and123.get() + " minus " + fractionNumber2and246.get() + " is: " +
+                fractionNumber4and123.sub(fractionNumber2and246).get());
+        System.out.println(fractionNumber4and123.get() + " multiply to  " + fractionNumber2and246.get() + " is: " +
+                fractionNumber4and123.mul(fractionNumber2and246).get());
+        System.out.println(fractionNumber4and123.get() + " divide of " + fractionNumber2and246.get() + " is: " +
+                fractionNumber4and123.div(fractionNumber2and246).get());
+
     }
 }
