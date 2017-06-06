ed133d149820b5fe5bd040eee9062d9ecca9db6e
Pavlo Letskyi <pavlo.letskyi@worldapp.com>
2017-06-06 18:43:55 +0300

    lesson6_HW_progress

---
+++ src/lesson6_homework/animals/Cat.java
@@ -0,0 +1,41 @@
+package lesson6_homework.animals;
+
+/**
+ * Created by pavlo.letskyi on 6/6/2017.
+ */
+public class Cat extends Animal {
+
+    String name;
+    boolean isHairRisen = false;
+    boolean isFed = false;
+/*Кошка должна уметь:
+— Вывести на экран имя, метод void printName()
+— Подать голос, метод void say()
+— Поднимать шерсть дыбом, метод void riseHair()
+— Питаться, метод void eat()
+— Орать если ненаелась, метод void yellIfNotGorged()*/
+
+    public void printName(){
+        System.out.println(name);
+    }
+
+    public void say(){
+        System.out.println("VOICE");
+    }
+
+    public void riseHair(){
+        isHairRisen = true;
+        System.out.println("HAIR RISEN");
+    }
+
+    public void eat(){
+        System.out.println("EATING");
+    }
+
+    public void yellIfNotGorged(){
+        if (!isFed){
+            System.out.println("YELLING! Feed me!");
+        }
+    }
+
+}
